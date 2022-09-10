4ac131dd44937c5ed545acbe1cee5c261fc7bbbe
Parth Tagadiya <parthtagadiya9199@gmail.com>
2022-09-10 13:01:57 +0530

    Add pulses

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import carousel1 from '../assets/images/slider-image/1.jpg'
+import carousel2 from '../assets/images/slider-image/2.jpg'
 
 function Header() {
     return (
         <>
             <div id="carouselExampleFade" className="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
-                <div className="carousel-indicators">
+            <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 </div>
                 <div className="carousel-inner h-100">
                     <div className="carousel-item active owl-item">
@@ -13,12 +16,30 @@ function Header() {
                         <div className="carousel-caption d-md-block slider-animated-1">
                             <h1 className="animated">
                             We Export<br />
-                                <strong>Precious Quality groceries</strong>
+                                <strong>Precious Quality Groceries</strong>
+                            </h1>
+                            <p className="animated">we aim to uniquely cater to your every need.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item owl-item">
+                        <img src={carousel2} className="d-block w-100 carousel-image-custom" alt="..." style={{ opacity: "0.8" }} />
+                        <div className="carousel-caption d-md-block slider-animated-1">
+                            <h1 className="animated">
+                            We Export<br />
+                                <strong>Precious Quality Pulses</strong>
                             </h1>
                             <p className="animated">we aim to uniquely cater to your every need.</p>
                         </div>
                     </div>
                 </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
         </>
     )
